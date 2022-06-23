33ec750121b29b5658f5f40b98819726440b9d75
ModasirQuayum <67706493+ModasirQuayum@users.noreply.github.com>
2022-06-23 22:31:23 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function menu(){
+
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav_menu');
+    
+    hamburger.addEventListener('click',()=>{
+        hamburger.classList.toggle("active")
+        navMenu.classList.toggle("active")
+    })
+    
+}
+menu();
